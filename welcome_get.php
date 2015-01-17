c9f548265c5455cae981019fb14596e125113902
rbhuta <rbhuta95@gmail.com>
2015-01-17 18:17:12 -0500

    Update welcome_get.php

--- welcome_get.php
+++ welcome_get.php
@@ -1,15 +1,7 @@
 <html>
 <body>
 
-Book: <?php echo $_GET["alpha"]; ?><br>
-Course Name: <?php echo $_GET["beta"]; ?><br>
-Professor: <?php echo $_GET["charlie"]; ?><br>
-ISBN: <?php echo $_GET["isbn"]; ?><br>
-Usefulness: <?php echo $_GET["response-rate1"]; ?><br>
-Bang: <?php echo $_GET["response-rate2"]; ?><br>
-Clarity: <?php echo $_GET["response-rate3"]; ?><br>
-Needit: <?php echo $_GET["switch1"]; ?><br>
-Comments: <?php echo $_GET["textarea"]; ?><br>
+Hello World.
 
 </body>
 </html>
